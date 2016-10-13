781d38a77fe34a8326aa48477dd00a81580aee02
thomasKneisel <thomas.kneisel@gmx.net>
2016-10-13 16:46:33 +0200

    autobuild-update

--- src/data.logger.js
+++ src/data.logger.js
@@ -1,24 +1,55 @@
 var dataLogger = {
 
-    logger: [],
-
-    log: function(type) {
-
+    logTypes: {
+        debug: -1,
+        success: 0,
+        info: 1,
+        warn: 2,
+        error: 3
     },
 
-    success: function() {
+    loggers: [
+        {
+            log: function(type, message) {
+                console.log(message)
+            }
+        }
+    ],
 
+    /**
+     * @param {string} type - message type
+     * @param {string} message - message text
+     */
+    log: function(type, message) {
+        _(this.loggers).forEach((logger) => logger.log(type, message))
     },
 
-    info: function() {
-
+    /**
+     * @param {string} message
+     */
+    success: function(message) {
+        this.log(this.logTypes.success, message);
     },
 
-    warn: function() {
-
+    /**
+     * @param {string} message
+     */
+    info: function(message) {
+        this.log(this.logTypes.info, message);
     },
 
-    error: function() {
+    /**
+     * @param {string} message
+     */
+    warn: function(message) {
+        this.log(this.logTypes.warn, message);
+    },
+
+    /**
+     * @param {string} message
+     */
+    error: function(message) {
+        this.log(this.logTypes.error, message);
     }
 }
 
--- tests/data.logger-test.js
+++ tests/data.logger-test.js
@@ -1,8 +1,12 @@
-var _ = require('lodash');
+_ = require('lodash'); // "Game" Situation
 var mockery = require('mockery');
 var chai = require('chai');
 var expect = chai.expect;
+
 var DataLogger = require('./../src/data.logger');
+
+var methods = ['log', 'success', 'info', 'warn', 'error'];
+var loggerMock = {};
 
 describe('DataLogger', function() {
     before(() => {
@@ -12,23 +16,62 @@ describe('DataLogger', function() {
         });
         console.log('\tStart Test Suite');
     });
-    // beforeEach(() => console.log('Start test'));
 
-    it('should be object', function() {
+    beforeEach(function() {
+        loggerMock = {
+            calledLog: false,
+            expectedType: 'mt',
+            expectedMsg: 'msg',
+            log: function(type, message) {
+                expect(type).is.equal(this.expectedType);
+                expect(message).is.equal(this.expectedMsg);
+                this.calledLog = true;
+            }
+        };
+    });
+
+    it('should be object', () => {
         expect(DataLogger).to.exist;
         expect(DataLogger).to.be.an('object');
     });
 
-    it('should have all log methods', function() {
-        var methods = ['log', 'success', 'info', 'warn', 'error'];
+    it('should have all log methods', () => {
         _(methods).forEach((methodName) => {
             expect(DataLogger[methodName]).exist;
             expect(DataLogger[methodName]).to.be.an('function')
         });
     });
 
+    it('log should call registerd loggers log method', () => {
+        mockery.registerMock('./../src/data.logger', loggerMock);
+        var someLogger = require('./../src/data.logger');
+
+        DataLogger.loggers.push(someLogger);
+        DataLogger.log('mt', 'msg');
+
+        expect(loggerMock.calledLog).is.ok;
+
+        mockery.deregisterMock('./../src/data.logger');
+    });
+
+    it('should use the right log types', () => {
+        var methods = ['success', 'info', 'warn', 'error'];
+
+        _(methods).forEach((methodName) => {
+            loggerMock.expectedType = DataLogger.logTypes[methodName];
+            mockery.registerMock('./../src/data.logger', loggerMock);
+            someLogger = require('./../src/data.logger');
+            DataLogger.loggers = [someLogger];
+
+            DataLogger[methodName]('msg');
+
+            mockery.deregisterMock('./../src/data.logger');
+        });
+    });
+
     // afterEach(() => console.log('End test'));
     after(() => {
+
         mockery.disable();
         console.log('End Test Suite');
     });
